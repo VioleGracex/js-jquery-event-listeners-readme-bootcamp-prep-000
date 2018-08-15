bf454b3b955a02843784717ef1ae86bb19f53e85
Viole Grace <highdevilxx@hotmail.com>
2018-08-15 10:07:35 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,3 +10,22 @@ function getIt(){
     window.alert("Hey!");
     });
   }
+  function frameIt(){
+    $("img").on("load", function(){
+      $("img").addClass("tasty");
+    });
+  }
+  
+  function pressIt(){
+    $("input").on("keydown", function(e){
+      if (e.which === 71){
+        window.alert("G was pressed.");
+      }
+    });
+  }
+  
+  function submitIt(){
+    $("form").on("submit", function(){
+      window.alert("Your form is going to be submitted now.");
+    });
+  }
