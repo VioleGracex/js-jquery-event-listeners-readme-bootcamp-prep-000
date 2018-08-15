5f6e53a1883a96bac31a56faf255fbcc755e8346
Viole Grace <highdevilxx@hotmail.com>
2018-08-15 09:26:18 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,3 +5,11 @@ $(document).ready(function(){
 // call functions here
 
 });
+function getIt()
+{
+  $(document.body).on('keydown', function(key) {
+  if(key.which == 83){
+      alert('s was pressed');
+  }
+});
+}
